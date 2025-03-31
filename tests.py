e677e42a73e9255f680974240a6d56f5b7daeb23
Gomina Aksinya <a.e.gomina@gmail.com>
2025-03-31 17:21:21 +0300

    Commit_2 исправлены ошибки в файле tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-@@ -18,7 +19,136 @@ def test_add_new_book_add_two_books(self):
+class TestBooksCollector:
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.books_genre) == 2
 
     # напиши свои тесты ниже
