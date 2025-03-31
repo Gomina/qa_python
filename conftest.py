2590daa5bf7d52c336f239127f51043863929cdf
Gomina Aksinya <a.e.gomina@gmail.com>
2025-03-31 16:55:46 +0300

    Commit_1 Сделаны тесты для BooksCollector Добавлено описание тестов в файл README

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from main import BooksCollector
+
+# словарь books_genre с книгами
+@pytest.fixture
+def modified_books_genre():
+    collector = BooksCollector()  # Создаем экземпляр класса BooksCollector
+    collector.books_genre = {
+        'Чемодан': 'Комедии',
+        'Война миров': 'Фантастика',
+        'Книга белой смерти': 'Ужасы',
+        'Ходячий замок': 'Фантастика',
+        'Легенды ночных стражей': None
+    }
+    yield collector
